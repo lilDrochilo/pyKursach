bb28fdc356780048b2a24319092aec3c86c8f88f
lilDrochilo <tim.bil.mix@gmail.com>
2023-12-16 15:08:01 +0300

    first try

--- kursovoy/kursovoy/settings.py
+++ kursovoy/kursovoy/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = "/Tatoo/mainpage/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
